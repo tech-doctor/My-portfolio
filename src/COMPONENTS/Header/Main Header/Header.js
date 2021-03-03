b5f002944207249c0cc008d574df01926ab8fe07
tech-doctor <55724699+tech-doctor@users.noreply.github.com>
2021-03-03 22:20:20 +0100

    updated src folder

--- src/COMPONENTS/Header/Main Header/Header.js
+++ src/COMPONENTS/Header/Main Header/Header.js
@@ -9,6 +9,8 @@ class Header extends Component {
     super(props)
       this.state = {
         Mobilevisible: false,
+        // NavIcon: 'fas fa-bars'
+
       }
   }
   
@@ -22,6 +24,11 @@ class Header extends Component {
    onClick = () => {
      this.setState({Mobilevisible: !this.state.Mobilevisible})
    }
+  //  closeNav = () => {
+  //    this.setState({NavIcon: 'fas fa-times'})
+  //  }
+
+
     
 
     render() {
@@ -38,7 +45,7 @@ class Header extends Component {
             <li className = "nav-content"><Link to = "/experience">Experience</Link></li>
             <li className = "nav-content"><Link to = "/project">Project</Link></li>
             <li className = "nav-content"><Link to = "/contact">Contact</Link></li>  
-            <button className = "fas fa-bars"
+            <button className = 'fas fa-bars'
              onClick = {this.onClick}
              style = {barStyle}
              >
